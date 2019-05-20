91ce0a76e526a7866232f29a561f9aa83bf28b13
practica <practica@pc-13-181>
2019-05-20 13:26:46 +0100

    un poco más

--- PI4/src/Ejercicio1/PDR.java
+++ PI4/src/Ejercicio1/PDR.java
@@ -11,6 +11,8 @@ public class PDR implements ProblemaPDR<List<Integer>, Integer,PDR>{
 	
 	private Integer index;
 	private List<Integer> lista;
+	private List<Integer> sum0;
+	private List<Integer> sum1;
 
 	public static Integer numeroBuscado(List<Integer> ls) {
 		return ls.stream()
@@ -55,23 +57,21 @@ public class PDR implements ProblemaPDR<List<Integer>, Integer,PDR>{
 	}
 
 	@Override
-	public PDR getSubProblema(Integer a) {//ramifiaciones del probelma un problema menor
-		PDR pro = this.clone();
-		pro.index = this.index + 1;
-		return pro;
-		
+	public PDR getSubProblema(Integer a) {//ramifiaciones del probelma un problema menor	
+		return null; //TODO
 	}
 
 	@Override
 	public Sp<Integer> getSolucionParcialPorAlternativa(Integer a, Sp<Integer> s) {
-		return Sp.create(a, s.propiedad + 1);
+		return Sp.create(a, s.propiedad + 1);//TODO
 	}
 
 	@Override
 	public List<Integer> getAlternativas() {
-//		List<Integer> alternativas = new ArrayList<Integer>();
-//		if(lista.get(this.index))
-		return null;
+		List<Integer> alternativas = new ArrayList<Integer>();
+		alternativas.add(0);
+		alternativas.add(1);
+		return alternativas;
  	}
 	@Override
 	public List<Integer> getSolucionReconstruidaCasoBase(Sp<Integer> sp) {
@@ -80,8 +80,56 @@ public class PDR implements ProblemaPDR<List<Integer>, Integer,PDR>{
 
 	@Override
 	public List<Integer> getSolucionReconstruidaCasoRecursivo(Sp<Integer> sp, List<Integer> s) {
-		s.add(0, sp.alternativa);
+		s.add(0, sp.alternativa); //TODO
 		return s;
+	}
+	
+	
+	
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((index == null) ? 0 : index.hashCode());
+		result = prime * result + ((lista == null) ? 0 : lista.hashCode());
+		result = prime * result + ((sum0 == null) ? 0 : sum0.hashCode());
+		result = prime * result + ((sum1 == null) ? 0 : sum1.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PDR other = (PDR) obj;
+		if (index == null) {
+			if (other.index != null)
+				return false;
+		} else if (!index.equals(other.index))
+			return false;
+		if (lista == null) {
+			if (other.lista != null)
+				return false;
+		} else if (!lista.equals(other.lista))
+			return false;
+		if (sum0 == null) {
+			if (other.sum0 != null)
+				return false;
+		} else if (!sum0.equals(other.sum0))
+			return false;
+		if (sum1 == null) {
+			if (other.sum1 != null)
+				return false;
+		} else if (!sum1.equals(other.sum1))
+			return false;
+		return true;
 	}
 
 	
