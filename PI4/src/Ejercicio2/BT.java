e1cf89cd4085f1d1397de9699bf8d3359d9c5fb6
albcorare <thealbertuswtf@gmail.com>
2019-05-21 14:29:26 +0200

    bt empezando

--- PI4/src/Ejercicio2/BT.java
+++ PI4/src/Ejercicio2/BT.java
@@ -1,5 +1,57 @@
 package Ejercicio2;
 
-public class BT {
+import java.util.List;
+
+import us.lsi.bt.EstadoBT;
+
+public class BT implements EstadoBT<List<Integer>, Integer, BT> {
+
+	@Override
+	public Tipo getTipo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BT getEstadoInicial() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BT avanza(Integer a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BT retrocede(Integer a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean esCasoBase() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Integer> getAlternativas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> getSolucion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
